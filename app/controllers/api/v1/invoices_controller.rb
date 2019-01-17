1042cd348672c57d828fa1aef2d433c892f0c810
Robin Borjesson <robin.b@outlook.com>
2019-01-17 12:53:57 +0100

    Add CRUD actions for invoice resource

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,13 +1,48 @@
 class Api::V1::InvoicesController < ApplicationController
   def index
+    @invoices = Card.all
+    render json: {invoices: @invoices}
   end
 
   def create
+    @invoice = Invoice.new(invoice_params)
+    
+    if @invoice.save      
+      render json: { invoice: @invoice }
+    else
+      render json: { error: @invoice.errors.full_messages }
+    end
   end
 
   def update
+    @invoice = Invoice.find(params[:id])
+    if @invoice.update(invoice_params)
+      render json: { invoice: @invoice }
+    else
+      render json: { error: @invoice.errors.full_messages }
+    end    
   end
 
-  def delete
+  def destroy
+
+    @invoice = Invoice.find(params[:id])
+    if @invoice.destroy
+
+      render json: {invoice: "invoice was successfully destroyed and removed from content packages they were included in"}
+    else
+      render json: {error: "Could not destroy invoice"}
+    end
+  end
+
+  private
+
+  def invoice_params
+    params.permit(:implementation_date, 
+                  :customer_id, 
+                  :reference,
+                  :issuer_id,
+                  :billed,
+                  :work_order,
+                  :project)
   end
 end
